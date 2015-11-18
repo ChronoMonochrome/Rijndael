0e95cb0ab165cc07b0623bd466a683041406a234
Shilin Victor <chrono.monochrome@gmail.com>
2015-11-19 00:49:58 +0200

    Add command line interface

--- main.cpp
+++ main.cpp
@@ -166,8 +166,126 @@ void decrypt(char *infile, char *outfile, char *privKey)
 
 int main(int argc, char *argv[])
 {
-	encrypt("orig.txt", "encrypted.txt", "public_key");
-	decrypt("encrypted.txt", "encrypted_message.txt", "private_key");
-	return 0;
+  cout << "AES/RSA hybrid cryptosystem\n"; 
+  cout << "Copyright (c) 2015 Shilin Victor\n\n";
+
+
+  try
+  {
+    if (argc < 4)
+      throw commandErr();
+
+
+    if (argv[1][0] == 'e')
+    {
+      if (argc == 4)
+      {
+        cryptkey publicKey, privateKey;
+
+
+        ifstream test (argv[2]);
+        if (!test.good())
+          throw fileErr(argv[2]);
+        test.close();
+
+
+	ifstream test1 ("public_key");
+	if (!test1.good()) {
+	        cout << "Generating keys...\n";
+	        rsa::genKeys(publicKey, privateKey);
+	        publicKey.saveToFile("public_key");
+	        privateKey.saveToFile("private_key");
+	}
+	test1.close();
+
+        cout << "Encrypting data...\n";
+	encrypt(argv[2], argv[3], "public_key");
+      }
+      else
+
+
+        if(argc == 5)
+        {
+          cryptkey publicKey;
+
+
+          if (!publicKey.loadFromFile(argv[4]))
+            throw fileErr(argv[4]);
+
+
+          cout << "Encrypting data...\n";
+          encrypt(argv[2], argv[3], argv[4]);
+        }
+        else
+          throw commandErr();
+    }
+    else
+      if (argv[1][0] == 'g')
+      {
+        if (argc == 4)
+        {
+          cryptkey publicKey, privateKey;
+          cout << "Generating keys...\n";
+
+
+          rsa::genKeys(publicKey, privateKey);
+
+
+          publicKey.saveToFile(argv[2]);
+          privateKey.saveToFile(argv[3]);
+        }
+        else
+          throw commandErr();
+      }
+      else
+        if (argv[1][0] == 'd')
+        {
+          if (argc == 5)
+          {
+            cryptkey privateKey;
+
+
+            if (!privateKey.loadFromFile(argv[4]))
+              throw fileErr(argv[4]);
+
+
+            cout << "Decrypting...\n";
+	    decrypt(argv[2], argv[3], argv[4]);
+          }
+          else
+            throw commandErr();
+        }
+        else
+          throw commandErr();
+
+
+  }
+  catch(rsaErr &err)
+  {
+    cout << "Error (rsa): " << err.what() <<endl;
+    return 1;
+  }
+  catch(mathErr &err)
+  {
+    cout << "Error (hugeint): " << err.what() <<endl;
+    return 1;
+  }
+  catch(commandErr)
+  {
+    cout << "Usage:\n";
+    cout << " e <in> <out> <public_key_path> - Encrypt data from the file <in> to <out>.\n";
+    cout << "    If the key is not specified, it will first try to use a file \"public_key\".\n";
+    cout << "    If this file is not found, it will be automatically created and saved\n";
+    cout << "    in the current directory.\n\n";
+    cout << " g <pb_key_path> <pr_key_path> - Generate keys.\n\n";
+    cout << " d <in> <out> <private_key_path> - Encrypt data from the file <in> to <out>.\n"; 
+  }
+  catch(fileErr &err)
+  {
+    cout << "Error: unable to open file " << err.filename() << endl;
+    return 1;
+  }
+
+  return 0;
 }
  
